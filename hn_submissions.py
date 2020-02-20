a57c89db04cab61513c762e8dbe0ff50d2c15aab
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-20 21:19:26 +0100

    Added functions to handle KeyError exception.

--- hn_submissions.py
+++ hn_submissions.py
@@ -2,6 +2,15 @@ from operator import itemgetter
 import requests
 import json
 
+def safe_execute(default, exception, function, arg):
+    try:
+        return function(arg)
+    except exception:
+        return default
+
+def return_response_dict(key):
+    return response_dict[key]
+
 # Make an API call and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 r = requests.get(url)
@@ -17,17 +26,15 @@ for submission_id in submission_ids[:30]:
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
+    with open(readable_file, 'a') as f:
+        json.dump(response_dict, f, indent=4)
 
     # Build a dictionary for each article.
-    try:
-        submission_dict = {
-            'title': response_dict['title'],
-            'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
-            'comments': response_dict['descendants'],
-        }
-    except KeyError:
-        with open(readable_file, 'w') as f:
-            json.dump(response_dict, f, indent=4)
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+        'comments': safe_execute(0, KeyError, return_response_dict, 'descendants'),
+    }
     submission_dicts.append(submission_dict)
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
@@ -37,10 +44,4 @@ for submission_dict in submission_dicts:
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
 
-# Make a separate API call for nr1 submission.
-url = f"https://hacker-news.firebaseio.com/v0/item/{submission_ids[0]}.json"
-r = requests.get(url)
-print(f"id: {submission_id}\tstatus: {r.status_code}")
-response_dict = r.json()
-with open(readable_file, 'a') as f:
-    json.dump(response_dict, f, indent=4)
+
