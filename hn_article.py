cf79f00c03afdf1675197fcd7323db3fcd0fd2f1
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-20 18:11:52 +0100

    Extracted and explored data.

--- hn_article.py
+++ hn_article.py
@@ -3,3 +3,8 @@ import requests
 url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
+
+# Process results.
+response_dict = r.json()
+for k, v in response_dict.items():
+    print(f"{k}: {v}")
